daadead7ef970a793ee09c728fd7c951e1a70c34
jcchau913 <85218807+jcchau913@users.noreply.github.com>
2021-08-09 16:33:31 -0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+
+function API_Album_Search(artist) {
+    var queryURL = "https://theaudiodb.com/api/v1/json/1/search.php?s=" + artist;
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) { 
+      var detailURL = "https://theaudiodb.com/api/v1/json/1/album.php?i=" + response.artists[0].idArtist;    
+      $.ajax({
+          url: detailURL,
+          method: "GET"
+        }).then(function(detail_response) {      
+          for (x=0; x<detail_response.album.length; x++) {
+            $("#album_list").append('<li><img src="' + detail_response.album[x].strAlbumThumb + '/preview"</img> ' + detail_response.album[x].strAlbum + ' (' + detail_response.album[x].intYearReleased + ')</li>');
+          }  
+        });
+    
+    });
+  }
+
+  function test() {
+      var albumArray = [];
+    $("body").prepend('<div id="testDiv"></div>')
+    $("#testDiv").append('<form><input type="text" id="artist-name"></input><button type="submit" id="search-button">Search Artist</button></form>');
+    $("#testDiv").append('<ul id="album_list"></ul>');
+    $("#search-button").on("click", function(e) {
+        e.preventDefault();
+        albumArray = API_Album_Search($("#artist-name").val());
+    });
+  }
+
+  test();
+
+  
