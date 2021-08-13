636b179527991ec6aaa832cb82ee79a27d7a9448
David Mendoza <davidmendoza0101@gmail.com>
2021-08-13 05:57:25 -0700

    Created a starting page for the website. Made it responsive and included an input field that takes the artist name. When the name is submitted, the starting page is hidden, then the band banner, album art carousel, and video carousel are produced dynamically and displayed. Also added a css file that formats sizes for various divs and adds a background image.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
-  
-
+//Function that takes the artist name and pulls data from the audiodb api to get album art
 function API_Album_Search(artist) {
   var queryURL = "https://theaudiodb.com/api/v1/json/1/search.php?s=" + artist;
-
+  console.log(queryURL);
   $.ajax({
     url: queryURL,
     method: "GET"
@@ -13,10 +12,10 @@ function API_Album_Search(artist) {
     $.ajax({
         url: detailURL,
         method: "GET"
+      //takes the api data and applies it to the album carousel 
       }).then(function(detail_response) {   
         $("#albumCarousel").empty();   
         for (x=0; x<detail_response.album.length; x++) {
-          //$("#album_list").append('<li><img src="' + detail_response.album[x].strAlbumThumb + '/preview"</img> ' + detail_response.album[x].strAlbum + ' (' + detail_response.album[x].intYearReleased + ')</li>');
           $(".carousel").append('<a class="carousel-item" id = "album' + x + '" href="#one!"><img src="' + detail_response.album[x].strAlbumThumb + '/preview"></a>');
         } 
         //remove the 'initialized' class which prevents slider from initializing itself again when it's not needed
@@ -29,34 +28,33 @@ function API_Album_Search(artist) {
   });
 }
 
-
+//Function that takes the artist name and pulls data from the audiodb api to get videos 
 function API_Video_Search(artist) {
   var queryURL = "https://theaudiodb.com/api/v1/json/1/search.php?s=" + artist;
-
+  console.log(queryURL);
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) { 
     var detailURL = "https://theaudiodb.com/api/v1/json/1/mvid.php?i=" + response.artists[0].idArtist;
-    $("#parallax-banner").attr("src",response.artists[0].strArtistBanner);  
-    $("#carousel-title").html("Videos");
+    console.log(detailURL)
+    $("#carousel-title2").html("Videos");
     $.ajax({
         url: detailURL,
         method: "GET"
+      ////takes the api data and applies it to the video carousel
       }).then(function(detail_response) {   
-        $("#albumCarousel").empty();
+        $("#videoCarousel").empty();
         console.log(detail_response);  
         for (x=0; x<detail_response.mvids.length; x++) {
-          //$("#album_list").append('<li><img src="' + detail_response.album[x].strAlbumThumb + '/preview"</img> ' + detail_response.album[x].strAlbum + ' (' + detail_response.album[x].intYearReleased + ')</li>');
-          //$(".carousel").append('<a class="carousel-item" id = "mvid' + x + '" href="' + detail_response.mvids[x].strMusicVid + '"><img src="' + detail_response.mvids[x].strTrackThumb + '"></a>');
-          $(".carousel").append('<div class="card carousel-item"><div class="card-content"><span class="card-title">' + detail_response.mvids[x].strTrack + '</span><a target=_blank href="' + detail_response.mvids[x].strMusicVid + '"><img src="' + detail_response.mvids[x].strTrackThumb + '" style="height:100%;width:100%;object-fit:cover"></a></div></div>');
+          $("#videoCarousel").append('<div class="card carousel-item"><div class="card-content"><span class="card-title">' + detail_response.mvids[x].strTrack + '</span><a target=_blank href="' + detail_response.mvids[x].strMusicVid + '"><img src="' + detail_response.mvids[x].strTrackThumb + '" style="height:100%;width:100%;object-fit:cover"></a></div></div>');
         } 
         //remove the 'initialized' class which prevents slider from initializing itself again when it's not needed
-        if ($(".carousel").hasClass('initialized')){
-          $(".carousel").removeClass('initialized')
+        if ($("#videoCarousel").hasClass('initialized')){
+          $("#videoCarousel").removeClass('initialized')
         }
         //just reinit the carousel
-        $(".carousel").carousel();          
+        $("#videoCarousel").carousel();          
       });
   });
 }
@@ -64,25 +62,37 @@ function API_Video_Search(artist) {
 
 
 function test() {
-  $("body").prepend('<div id="testDiv"></div>')
-  $("#testDiv").append('<input type="text" id="artist-name">');
-  $("#testDiv").append('</input><button type="submit" id="search-album">Album Search</button>');
-  $("#testDiv").append('</input><button type="submit" id="search-video">Video Search</button>');
-  $("#testDiv").append('<h4 id="carousel-title"></h4>');
-  $("#search-album").on("click", function(e) {
-      e.preventDefault();
-      API_Album_Search($("#artist-name").val());
-  });
-  $("#search-video").on("click", function(e) {
-    e.preventDefault();
-    API_Video_Search($("#artist-name").val());
-});  
-  $("#testDiv").append('<div class="parallax"><img id="parallax-banner" src="" alt=""></div>');
+  
+  //function that is called when the enter key is pressed while inside the input field
+  $(function(){
+    $("#artistInput").keyup(function (e) {
+      if (e.which == 13) {   
+        $('input[name="textInput"]').trigger('click');
+        //Hides the starting screen
+        $("#col1, #col2").hide();
+        //Shows the rows that are going to have content inserted 
+        $("#row3, #row4, #row5").removeClass("hide");
+        //removes the limits on vertical scrolling;
+        $("body").removeClass("screenLimit");
+        //Appends the div containing the band banner to col3
+        $("#col3").append('<div class="parallax"><img id="parallax-banner" src="" alt=""></div>');
+        //sets the variable artist to be the value of what is inside the input div
+        artist=$("#icon_prefix2").val();
+        //calls the function with the input 'artist' that generates the album art carousel
+        API_Album_Search(artist);
+        //calls the function with the input 'artist' that generates the artist video carousel
+        API_Video_Search(artist);
+        e.preventDefault();
+       
+      }
+    });
+  }); 
+ 
 }
 
 test();
 
-  
+//initializes the carousels
 $(document).ready(function(){
   $('.carousel').carousel();
 });
